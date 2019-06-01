270a15f451028051538ed2a96dc8c578bc55e6a5
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-06-01 20:55:44 +0200

    Delete files

--- templates/vendor/parts/message-button.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
-?>
-<button type="button" class="hp-vendor__action hp-js-link alt" data-url="#hp-<?php if ( is_user_logged_in() ) : ?>message-send-<?php echo esc_attr( $vendor->ID ); else : ?>user-login<?php endif; ?>" data-type="popup"><?php esc_html_e( 'Send Message', 'hivepress-messages' ); ?></button>
-<?php echo hivepress()->template->render_part( 'vendor/parts/message-popup', [ 'vendor' => $vendor ] );
--- templates/vendor/parts/message-link.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
-?>
-<a href="#hp-<?php if ( is_user_logged_in() ) : ?>message-send-<?php echo esc_attr( $vendor->ID ); else : ?>user-login<?php endif; ?>" title="<?php esc_attr_e( 'Send Message', 'hivepress-messages' ); ?>" class="hp-vendor__action hp-js-link" data-type="popup"><i class="hp-icon fas fa-comment"></i></a>
-<?php echo hivepress()->template->render_part( 'vendor/parts/message-popup', [ 'vendor' => $vendor ] ); ?>
