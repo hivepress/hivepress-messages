453dddf09c064fe5425bbfa792afbfaa9e4bcd6d
Ihor <ihor@hivepress.io>
2020-07-27 18:40:20 +0300

    Fix translation strings

--- templates/vendor/view/block/message-send-link.php
+++ templates/vendor/view/block/message-send-link.php
@@ -2,4 +2,4 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<a href="#<?php if ( is_user_logged_in() ) : ?>message_send_modal_<?php echo esc_attr( $vendor->get_id() ); else : ?>user_login_modal<?php endif; ?>" title="<?php esc_attr_e( 'Send Message', 'hivepress-messages' ); ?>" class="hp-vendor__action hp-vendor__action--message"><i class="hp-icon fas fa-comment"></i></a>
+<a href="#<?php if ( is_user_logged_in() ) : ?>message_send_modal_<?php echo esc_attr( $vendor->get_id() ); else : ?>user_login_modal<?php endif; ?>" title="<?php echo esc_attr( hivepress()->translator->get_string( 'send_message' ) ); ?>" class="hp-vendor__action hp-vendor__action--message"><i class="hp-icon fas fa-comment"></i></a>
