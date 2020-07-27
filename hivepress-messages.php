64fd34e556beadd46083de6fd7469ae2eb67f9cd
Ihor <ihor@hivepress.io>
2020-07-27 18:33:37 +0300

    Update version

--- hivepress-messages.php
+++ hivepress-messages.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: HivePress Messages
  * Description: Allow users to send private messages.
- * Version: 1.2.3
+ * Version: 1.2.4
  * Author: HivePress
  * Author URI: https://hivepress.io/
  * Text Domain: hivepress-messages
