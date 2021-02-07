157eac5332120e849b587987c9cf8485f8a3da28
Ihor <ihor@hivepress.io>
2021-02-08 00:39:14 +0200

    Add unread icon

--- templates/message/thread/message-sender.php
+++ templates/message/thread/message-sender.php
@@ -3,5 +3,8 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 <td class="hp-message__sender">
-	<a href="<?php echo esc_url( hivepress()->router->get_url( 'messages_view_page', [ 'user_id' => $message->get_sender__id() ] ) ); ?>" class="hp-link"><i class="hp-icon fas fa-share"></i><span><?php echo esc_html( $message->get_sender__display_name() ); ?></span></a>
+	<a href="<?php echo esc_url( hivepress()->router->get_url( 'messages_view_page', [ 'user_id' => $message->get_sender__id() ] ) ); ?>" class="hp-link">
+		<i class="hp-icon fas fa-envelope<?php if ( $message->is_read() ) : ?>-open<?php endif; ?>"></i>
+		<span><?php echo esc_html( $message->get_sender__display_name() ); ?></span>
+	</a>
 </td>
