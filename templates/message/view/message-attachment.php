c713ea8360c7d0dc49ec4271819c048404683369
Ihor <ihor@hivepress.io>
2021-04-06 20:28:17 +0300

    Fix attachment name

--- templates/message/view/message-attachment.php
+++ templates/message/view/message-attachment.php
@@ -6,7 +6,7 @@ if ( $message->get_attachment__id() ) :
 	?>
 	<a href="<?php echo esc_url( $message->get_attachment__url() ); ?>" target="_blank" class="hp-message__attachment hp-link">
 		<i class="hp-icon fas fa-file-download"></i>
-		<span><?php echo esc_html( wp_basename( $message->get_attachment__url() ) ); ?></span>
+		<span><?php echo esc_html( $message->get_attachment__name() ); ?></span>
 	</a>
 	<?php
 endif;
