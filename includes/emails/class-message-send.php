e0395d05f2623e64917ad938f26661cc68c1a586
Ihor <ihor@hivepress.io>
2021-06-12 15:47:24 +0300

    Fix email compatibility

--- includes/emails/class-message-send.php
+++ includes/emails/class-message-send.php
@@ -20,6 +20,25 @@ defined( 'ABSPATH' ) || exit;
 class Message_Send extends Email {
 
 	/**
+	 * Class initializer.
+	 *
+	 * @param array $meta Form meta.
+	 */
+	public static function init( $meta = [] ) {
+		$meta = hp\merge_arrays(
+			[
+				'label'       => esc_html__( 'Message Received', 'hivepress-messages' ),
+				'description' => esc_html__( 'This email is sent to users when a new message is received.', 'hivepress-messages' ),
+				'recipient'   => hivepress()->translator->get_string( 'user' ),
+				'tokens'      => [ 'user_name', 'message_url', 'message_text', 'message', 'sender', 'recipient' ],
+			],
+			$meta
+		);
+
+		parent::init( $meta );
+	}
+
+	/**
 	 * Class constructor.
 	 *
 	 * @param array $args Email arguments.
@@ -28,6 +47,7 @@ class Message_Send extends Email {
 		$args = hp\merge_arrays(
 			[
 				'subject' => hp\sanitize_html( __( 'Message Received', 'hivepress-messages' ) ),
+				'body'    => hp\sanitize_html( __( "Hi, %user_name%! You've received a new message, click on the following link to view it: %message_url%", 'hivepress-messages' ) ),
 			],
 			$args
 		);
