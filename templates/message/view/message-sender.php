35a1da65b1ed755b50371873bfdad7aeeb1f4caf
kuznev <kuznietevhen@gmail.com>
2023-02-15 16:17:48 +0200

    Minor fixes

--- templates/message/view/message-sender.php
+++ templates/message/view/message-sender.php
@@ -1,10 +1,9 @@
 <?php
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-$sender = $message->get_sender();
-$display = get_option( 'hp_user_enable_display' ) && $sender && get_current_user_id() !== $sender->get_id();
+$display = get_option( 'hp_user_enable_display' ) && $message->get_sender() && get_current_user_id() !== $message->get_sender__id();
 if ( $display ) : ?>
-<a class="hp-link hp-message__sender" href="<?php echo esc_url( hivepress()->router->get_url( 'user_view_page', [ 'username' => $sender->get_username() ] ) ); ?>">
+<a class="hp-link hp-message__sender" href="<?php echo esc_url( hivepress()->router->get_url( 'user_view_page', [ 'username' => $message->get_sender__username() ] ) ); ?>">
 <?php endif; ?>
 <strong <?php if ( ! $display ) : ?>class="hp-message__sender"<?php endif; ?>><?php echo esc_html( $message->get_sender__display_name() ); ?></strong>
 <?php if ( $display ) : ?></a><?php endif; ?>
