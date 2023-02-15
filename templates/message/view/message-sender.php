90cac5931d4fa7cf856b64e8871a9f67f77dc5b2
kuznev <kuznietevhen@gmail.com>
2023-02-15 15:43:39 +0200

    Make usernames clickable

--- templates/message/view/message-sender.php
+++ templates/message/view/message-sender.php
@@ -1,5 +1,10 @@
 <?php
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-?>
-<strong class="hp-message__sender"><?php echo esc_html( $message->get_sender__display_name() ); ?></strong>
+$sender = $message->get_sender();
+$display = get_option( 'hp_user_enable_display' ) && $sender && get_current_user_id() !== $sender->get_id();
+if ( $display ) : ?>
+<a class="hp-link hp-message__sender" href="<?php echo esc_url( hivepress()->router->get_url( 'user_view_page', [ 'username' => $sender->get_username() ] ) ); ?>">
+<?php endif; ?>
+<strong <?php if ( ! $display ) : ?>class="hp-message__sender"<?php endif; ?>><?php echo esc_html( $message->get_sender__display_name() ); ?></strong>
+<?php if ( $display ) : ?></a><?php endif; ?>
