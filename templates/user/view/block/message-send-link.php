336c3c89209c23bb41e1ba07e70c558c578b0663
HivePress <43493187+hivepress@users.noreply.github.com>
2024-12-18 22:12:16 +0100

    Fix formatting

--- templates/user/view/block/message-send-link.php
+++ templates/user/view/block/message-send-link.php
@@ -2,12 +2,4 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<a href="#
-<?php
-if ( is_user_logged_in() ) :
-	?>
-	message_send_modal_
-	<?php
-	echo esc_attr( $user->get_id() ); else :
-		?>
-	user_login_modal<?php endif; ?>" title="<?php echo esc_attr( hivepress()->translator->get_string( 'send_message' ) ); ?>" class="hp-vendor__action hp-vendor__action--message"><i class="hp-icon fas fa-comment"></i></a>
+<a href="#<?php if ( is_user_logged_in() ) : ?>message_send_modal_<?php echo esc_attr( $user->get_id() ); else : ?>user_login_modal<?php endif; ?>" title="<?php echo esc_attr( hivepress()->translator->get_string( 'send_message' ) ); ?>" class="hp-vendor__action hp-vendor__action--message"><i class="hp-icon fas fa-comment"></i></a>
